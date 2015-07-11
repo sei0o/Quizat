d9a88c0822ab99e66ad2915e9143defcf77db273
sei0o <sei0o@live.jp>
2015-07-11 20:21:04 +0900

    Practice Bootstrap

--- app.rb
+++ app.rb
@@ -2,7 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/flash'
 require 'active_record'
-require 'slim'
+
+# see: https://gist.github.com/mamantoha/3358074
+module Sinatra
+  module Flash
+    module Style
+      def styled_flash(key=:flash)
+        return "" if flash(key).empty?
+        id = (key == :flash ? "flash" : "flash_#{key}")
+        close = '<a class="close" data-dismiss="alert" href="#">×</a>'
+        messages = flash(key).collect {|message| "  <div class='alert alert-#{message[0]}'>#{close}\n #{message[1]}</div>\n"}
+        "<div id='#{id}'>\n" + messages.join + "</div>"
+      end
+    end
+  end
+end
 
 class Quizat < Sinatra::Base
   configure do
@@ -17,5 +31,9 @@ class Quizat < Sinatra::Base
 
   get "/" do
     erb :index
+  end
+
+  get "/signup" do
+    erb :signup
   end
 end
